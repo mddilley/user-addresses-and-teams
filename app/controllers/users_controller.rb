166c25a8e982d0cdc7aaa5bcb54a9b6611151c3a
Michael Dilley <mike.dilley@gmail.com>
2019-01-23 14:08:02 -0600

    Add fields_for :addresses to new user form, add addresses builds to new action in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def new
     @user = User.new
+    @user.addresses.build(:address_type => "Home")
+    @user.addresses.build(:address_type => "Business")
   end
 
   def create
